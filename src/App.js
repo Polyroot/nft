2452c7439989ef88b784bc1773d54aaf5a200c3b
chat <lastofdev@yandex.ru>
2022-07-31 13:47:45 +0300

    add useState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
-import { HomePage } from './pages/HomePage/HomePage'
 import { RouterApp } from './router/RouterApp';
 
 function App() {
   return (
-    // <div>
-    //   <Navbar />
-    //   <HomePage />
-    // </div>
     <RouterApp>
       <Navbar />
     </RouterApp>
--- src/pages/CreatePage/CreatePage.js
+++ src/pages/CreatePage/CreatePage.js
@@ -1,24 +1,44 @@
+import React from 'react';
 import styles from './CreatePage.module.scss'
 import { Button } from '../../components/Button/Button';
 import { useState } from 'react';
 
 export function CreatePage() {
+    const [inputname, setName] = useState('');
+    const [desription, setdesription] = useState('');
+
+    function handleName(e) {
+        setName(e.currentTarget.value);
+    }
+
+    function handleDesription(e) {
+        setdesription(e.currentTarget.value);
+    }
+
+    function handleCreate() {
+
+    }
 
     return (
         <div className={styles["container"]}>
+            {console.log(inputname, desription)}
             <div className={styles["form"]}>
                 <span className={styles["title"]}>Create new item</span>
                 <br />
                 <div className={styles["uploader"]}> Вставь сюда картинку</div>
                 <br />
-                <input placeholder='name' />
+                <input
+                    placeholder='name'
+                    onChange={handleName}
+                    value={inputname} />
                 <br />
-                <input placeholder='discription' />
+                <input
+                    placeholder='discription'
+                    onChange={handleDesription}
+                    value={desription} />
                 <br />
-                <Button title='Create' />
+                <Button title='Create' onClick={handleCreate} />
             </div>
-
-
         </div>
     );
 }
