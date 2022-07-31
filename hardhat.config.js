0f9b7f817d6d3ef8809c9a8812aba8900caaee5f
chat <lastofdev@yandex.ru>
2022-07-31 17:52:24 +0300

    setting-hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,19 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
+  // defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
 };
